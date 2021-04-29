4772fd4f8026ab70bbdfe18a9b81318e9e084cd1
mylescarpenter <myles9702>
2021-04-29 13:04:52 -0500

    Started LinkedList class

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -1,5 +1,17 @@
 public class SinglyLinkedList {
 
+    ListNode first;
+    ListNode last;
 
+    public SinglyLinkedList() {
 
+    }
+
+    public SinglyLinkedList(ListNode node){
+        add(node);
+    }
+
+    public static SinglyLinkedList add(ListNode node){
+
+    }
 }
